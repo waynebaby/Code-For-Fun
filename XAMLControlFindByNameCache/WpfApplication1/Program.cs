15027d8e85153ec618e622c82040cb4df74f6e9c
Wayne Wang 韦恩卑鄙 <blackshaman_wayne@hotmail.com>
2012-08-28 01:29:49 +0800

    一个获得所有设置了名字的Control的引用缓存 用于按照名字查找
    用linkedlist是为了在循环的时候从集合中删除  不用linked list 用hashset也没什么问题
    还可以在改名的时候从集合中移除。但是一般不太会有这样的情况发生

---
+++ XAMLControlFindByNameCache/WpfApplication1/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApplication1
+{
+
+    public  class AllControlNameCache
+    {
+        static AllControlNameCache()
+        { 
+            
+        }
+
+        static Dictionary<string, LinkedList<WeakReference<FrameworkElement>>> controlsCache
+            = new Dictionary<string, LinkedList<WeakReference<FrameworkElement>>>();
+        public static void InitCache()
+        {
+            var nmeta = FrameworkElement.NameProperty.GetMetadata(DependencyObjectType.FromSystemType(typeof(FrameworkElement)));
+            var fi = typeof(PropertyMetadata).GetField("_propertyChangedCallback", BindingFlags.NonPublic | BindingFlags.Instance);
+            var newcb = new PropertyChangedCallback((o, e) =>
+            {
+                //MessageBox.Show(string.Format("object:{0},\r\nname:{1}", o.ToString(), e.NewValue.ToString()));
+                LinkedList<WeakReference<FrameworkElement>> targetList;
+                var key=e.NewValue.ToString();
+                targetList = GetListByKey(key);
+                targetList.AddLast(new WeakReference <FrameworkElement >(o as FrameworkElement));
+            });
+            fi.SetValue(nmeta, newcb);
+
+        }
+
+        private static LinkedList<WeakReference<FrameworkElement>> GetListByKey(string key)
+        {
+            LinkedList<WeakReference<FrameworkElement>> targetList;
+            if (!controlsCache.TryGetValue(key, out targetList))
+            {
+                targetList = new LinkedList<WeakReference<FrameworkElement>>();
+                controlsCache[key] = targetList;
+            }
+            return targetList;
+        }
+
+        public static IEnumerable<FrameworkElement> GetControls(string name)
+        {
+            var lst = GetListByKey(name);
+            var node = lst.First;
+            while (node!=null)
+            {
+                var next = node.Next;
+                FrameworkElement target;
+                
+                if (node.Value.TryGetTarget(out target))
+                {
+                    yield return target;
+                }
+                else
+                {
+                    lst.Remove(node);                
+                }
+                node = next;
+            }
+        
+        }
+    }
+
+    class Program
+    {
+        [System.STAThreadAttribute()]
+
+        public static void Main()
+        {        
+            AllControlNameCache.InitCache();
+            WpfApplication1.App app = new WpfApplication1.App();
+            app.InitializeComponent();
+            app.Run();
+        }
+    }
+}
