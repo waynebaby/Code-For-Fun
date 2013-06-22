7b0b63ec4b7e847eab5dac64d67d281df9b0d781
waynebaby <blackshaman_wayne@hotmail.com>
2013-06-22 17:48:59 +0800

    Design

--- AzureChat/AzureChat.Abstractions/IPostOfficeService.cs
+++ AzureChat/AzureChat.Abstractions/IPostOfficeService.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AzureChat.Abstractions
 {
+ 
     public interface IPostOfficeService :
         IObserver<ServiceRegisterNotifyMessage<IUser<IUserData, IUserStatus, IInbox, IOutbox>>>,
         IObserver<ServiceRegisterNotifyMessage<IChannel<IChannelData, IChannelStatus>>>,
@@ -19,8 +21,11 @@ namespace AzureChat.Abstractions
         //Task<IPostOfficeService> LocateUserHubServiceAsync(Guid id);
         //Task<IChannelHubService> LocateChannelHubServiceAsync(Guid id);
         //Task<IPostOfficeService> LocatePostOfficeServiceAsync(Guid id);
+ 
         Task<IUser<IUserData, IUserStatus, IInbox, IOutbox>> LocateUserAsync(Guid id);
+
         Task<IChannel<IChannelData, IChannelStatus>> LocateChannelAsync(Guid id);
+
         IUserHubService LocalUserHubService { get; }
         IChannelHubService LocalChannelHubService { get; }
         bool IsRemote { get; }
