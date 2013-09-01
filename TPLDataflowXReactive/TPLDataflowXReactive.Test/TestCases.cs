4382e0c8da4bc89e5fc8b3f861ff0e0355bdc153
waynebaby <blackshaman_wayne@hotmail.com>
2013-09-01 17:53:40 +0800

    add some test of TPL Dataflow &  IObervable

---
+++ TPLDataflowXReactive/TPLDataflowXReactive.Test/TestCases.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TPLDataflowXReactive.Test
+{
+    [TestClass]
+    public class TestCases
+    {
+        [TestMethod]
+        public async Task TestBufferBlock_AsyncIn_ObservableOut()
+        {
+
+            var outputList = new List<int>();
+            var bcontext = new TPLDataflowXReactive.TPLContextDefault<BufferBlock<int>, int>();
+            var task = new Task(() => { });
+            var d1 = bcontext.CurrentObservable.Subscribe(e =>
+                {
+                    outputList.Add(e);
+                    task.Start();
+                });
+
+            await bcontext.CurrentBlock.SendAsync(1);
+            await task;
+            Assert.AreEqual(outputList[0], 1);
+
+
+
+        }
+
+        [TestMethod]
+        public async Task TestBufferBlock_ObserverIn_AsyncOut()
+        {
+
+
+            var bcontext = new TPLDataflowXReactive.TPLContextDefault<BufferBlock<int>, int>();
+            bcontext.CurrentObserver.OnNext(1);
+
+            var outval = await bcontext.CurrentBlock.ReceiveAsync();
+
+
+            Assert.AreEqual(outval, 1);
+
+
+
+        }
+
+
+
+        [TestMethod]
+        public async Task TestBufferBlock_ObserverIn_AsyncObservableBothOut()
+        {
+
+
+            var outputList = new List<int>();
+            var bcontext = new TPLDataflowXReactive.TPLContextDefault<BufferBlock<int>, int>();
+            var task = new Task(() => { });
+
+
+            bcontext.CurrentObserver.OnNext(1);
+
+            var outval = await bcontext.CurrentBlock.ReceiveAsync();
+            Assert.AreEqual(outval, 1);
+
+
+            var d1 = bcontext.CurrentObservable.Subscribe(e =>
+            {
+                outputList.Add(e);
+                task.Start();
+            });
+
+            bcontext.CurrentObserver.OnNext(2);
+
+            await task;
+            Assert.AreEqual(outputList[0], 2);
+
+            task = new Task(() => { });
+            var toutval2 = bcontext.CurrentBlock.ReceiveAsync();
+            bcontext.CurrentObserver.OnNext(3);
+
+            Assert.AreEqual(outputList[1], 3);
+            //outval = await toutval2;  <== if observer is attached, the receive async will never work; this line will idle the process
+
+
+        }
+    }
+}
---
+++ TPLDataflowXReactive/TPLDataflowXReactive/TPLContext.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace TPLDataflowXReactive
+{
+    public abstract class TPLContextBase<T>
+    {
+        IPropagatorBlock<T, T>
+            _CurrentBlock;
+
+        public System.Threading.Tasks.Dataflow.IPropagatorBlock<T, T> CurrentBlock
+        {
+            get { return _CurrentBlock; }
+            set
+            {
+                _CurrentBlock = value;
+                CurrentObservable = CurrentBlock.AsObservable();
+                CurrentObserver = CurrentBlock.AsObserver();
+
+            }
+        }
+
+        public IObserver<T> CurrentObserver { get; private set; }
+        public IObservable<T> CurrentObservable { get; private set; }
+
+
+    }
+
+
+    public class TPLContextDefault<TBlock, TValue> : TPLContextBase<TValue> where TBlock : IPropagatorBlock<TValue, TValue>, new()
+    {
+        public TPLContextDefault()
+        {
+            CurrentBlock = new TBlock();
+        }
+
+    }
+}
